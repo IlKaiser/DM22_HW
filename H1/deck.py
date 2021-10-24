6870f00c16e7e77ee7574d18562bd46dee078b84
Marco Calamo (IlKaiser) <samuele.rinaldi90@gmail.com>
2021-10-24 19:03:51 +0200

    Small fixes for 1st delivery

--- H1/deck.py
+++ H1/deck.py
@@ -4,7 +4,7 @@ import pydealer
 import matplotlib.pyplot as plt
 import numpy as np
 
-trials = 100000  # ~3 mins for a million trials
+trials = 1000000  # ~3 mins for a million trials
 FORMAT = '{0:.5f}'
 
 c_at_least_1_ace_3_cards = 0
@@ -14,6 +14,10 @@ c_all_diamonds_5_cards = 0
 c_full_5_cards = 0
 
 al1a3c = []
+ae3c   = []
+e1a5c  = []
+ad5c   = []
+f5c    = []
 
 
 def at_least_1_ace_3_cards(cards):
@@ -71,12 +75,33 @@ for i in range(0, trials):
     if full_5_cards(hand):
         c_full_5_cards += 1
     al1a3c.append(c_at_least_1_ace_3_cards/(i+1))
+    ae3c.append(c_all_equals_3_cards/(i+1))
+    e1a5c.append(c_exactly_1_ace_5_cards/(i+1))
+    ad5c.append(c_all_diamonds_5_cards/(i+1))
+    f5c.append(c_full_5_cards/(i+1))
     
 print_results()
 diff = time.time() - start_time
 print("\n--- %s seconds elapsed---" % (diff))
 print("\n--- %s seconds per trial ---" % (diff/trials))
 
-fig, ax = plt.subplots()  # Create a figure containing a single axes.
-ax.plot(np.arange(trials),al1a3c )  # Plot some data on the axes.
+fig = plt.figure()
+gs = fig.add_gridspec(5, hspace=0.5)
+axs = gs.subplots(sharex=True)
+
+axs[0].plot(np.arange(trials),al1a3c )  # Plot some data on the axes.
+axs[0].set_title("At least one ace in 3 cards ")
+
+axs[1].plot(np.arange(trials),ae3c) 
+axs[1].set_title("First 3 cards are the same")
+
+axs[2].plot(np.arange(trials),e1a5c ) 
+axs[2].set_title("Exactly one ace in 5 cards ")
+
+axs[3].plot(np.arange(trials),ad5c ) 
+axs[3].set_title("All diamond in 5 cards ")
+
+axs[4].plot(np.arange(trials),f5c ) 
+axs[4].set_title("Full in 5 cards")
+
 plt.show()
